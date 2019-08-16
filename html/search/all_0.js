b719d42bb9b04f7de00b1c08c5654daaa1e18c70
caternuson <methuzla@speakeasy.net>
2019-08-15 22:41:45 -0700

    doxy test

---
+++ html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['crgb_0',['cRGB',['../structc_r_g_b.html',1,'']]],
+  ['crgbw_1',['cRGBW',['../structc_r_g_b_w.html',1,'']]]
+];
---
+++ html/search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['crgb_2',['cRGB',['../structc_r_g_b.html',1,'']]],
+  ['crgbw_3',['cRGBW',['../structc_r_g_b_w.html',1,'']]]
+];
